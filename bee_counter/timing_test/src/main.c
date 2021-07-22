e9b14c8dcd007be2ca2ec58f79a6b2336f56138e
kim-ado <mikdoan99@gmail.com>
2021-07-22 14:01:01 +0200

    Edited it to smooth out function

--- bee_counter/timing_test/src/main.c
+++ bee_counter/timing_test/src/main.c
@@ -5,29 +5,29 @@ Dank code for the dank people @ nordicsemi
 #include <zephyr.h>
 #include <sys/printk.h>
 #include <timing/timing.h>
-#include <device.h>
+#include <stdio.h>
 
-//Lag denne timer greia ferdig, litt stress men ok
+timing_t start_time, end_time;
+unsigned long milliseconds_spent;
+unsigned long two_seconds;
 
-
-struct timer{
-	uint64_t start_time;
-	uint64_t current_time;
-	int time_in_ns;
-};
+unsigned long current_time(timing_t *start_time, timing_t *end_time){ 
+    *end_time = timing_counter_get(); 
+	unsigned long current_time_ns = timing_cycles_to_ns(timing_cycles_get(start_time, end_time)) * 0.000001; //Converts to ms
+	return current_time_ns; 
+}
 
 void main(void){
-		timing_init();
-    	timing_start();
+	timing_init();
+	timing_start();
+	while(1){
 		start_time = timing_counter_get();
-		printk("Time after initilization: %lld\n", start_time);
 		k_busy_wait(1000000);
-		current_time = timing_counter_get();
-		real_end_time = timing_cycles_to_ns(timing_cycles_get(&start_time, &current_time));
-		printk("Time after 1 second: %d\n", real_end_time);
+		milliseconds_spent = current_time(&start_time, &end_time);
 		k_busy_wait(1000000);
-		current_time = timing_counter_get();
-		real_end_time = timing_cycles_to_ns(timing_cycles_get(&start_time, &current_time));
-		printk("Time after 2 seconds: %d\n", real_end_time);
+		two_seconds = current_time(&start_time, &end_time);
+		printf("Milliseconds spent: %ld \n", milliseconds_spent);
+		printf("Two seconds: %ld \n", two_seconds);
+	}
 }
 
