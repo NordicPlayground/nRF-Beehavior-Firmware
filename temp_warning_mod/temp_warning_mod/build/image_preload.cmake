a11dbaf48fe9eca57809c5eb991598fb9356ffcf
Morten K Hovstad <morten.hovstad@nordicsemi.no>
2022-03-31 14:08:34 +0200

    Init project

---
+++ temp_warning_mod/temp_warning_mod/build/image_preload.cmake
@@ -0,0 +1,6 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/usr/bin/python3" CACHE INTERNAL "NCS child image controlled")
---
+++ temp_warning_mod/temp_warning_mod/build/zephyr/cmake_install.cmake
@@ -0,0 +1,239 @@
+# Install script for directory: /home/moho/ncs/zephyr
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/home/moho/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux/gcc-arm-none-eabi-10.3-2021.10/bin/arm-none-eabi-objdump")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/zephyr/arch/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/zephyr/lib/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/zephyr/soc/arm/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/zephyr/boards/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/zephyr/subsys/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/zephyr/drivers/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/nrf/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/mcuboot/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/mbedtls/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/trusted-firmware-m/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/cjson/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/cddl-gen/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/memfault-firmware-sdk/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/canopennode/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/civetweb/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/cmsis/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/fatfs/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/hal_nordic/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/st/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/libmetal/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/littlefs/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/loramac-node/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/lvgl/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/lz4/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/mipi-sys-t/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/nanopb/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/nrf_hw_models/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/open-amp/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/openthread/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/segger/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/tinycbor/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/tinycrypt/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/TraceRecorder/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/zscilib/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/nrfxlib/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/modules/connectedhomeip/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/zephyr/kernel/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/zephyr/cmake/flash/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/zephyr/cmake/usage/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/moho/Documents/nRF-Beehavior-Firmware/temp_warning_mod/temp_warning_mod/build/zephyr/cmake/reports/cmake_install.cmake")
+endif()
+
