53967f53e9faf692e56dafdce1032a3fcc287144
mortekho <101815514+mortekho@users.noreply.github.com>
2022-06-21 08:10:59 +0200

    Remove build dir

--- v2.0Woodpecker/build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD_ROOT "/home/moho/Documents/nRF-Beehavior-Firmware/v2.0Woodpecker;/home/moho/ncs/nrf;/home/moho/ncs/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "/home/moho/Documents/nRF-Beehavior-Firmware/v2.0Woodpecker/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "/home/moho/Documents/nRF-Beehavior-Firmware/v2.0Woodpecker/app.overlay" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "/home/moho/Documents/nRF-Beehavior-Firmware/v2.0Woodpecker/app.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "/home/moho/ncs/toolchains/v2.0.0/usr/local/bin/python3.8" CACHE INTERNAL "NCS child image controlled")
-set(mcuboot_DTC_OVERLAY_FILE ";/home/moho/ncs/nrf/modules/mcuboot/usb.overlay" CACHE INTERNAL "NCS child image controlled")
