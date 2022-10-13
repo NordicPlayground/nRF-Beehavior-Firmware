d73aaacd2378488bca89f5ec55282599730395a5
Morten Klingan Hovstad <101815514+mortekho@users.noreply.github.com>
2022-10-13 17:01:36 +0200

    Delete Legacy_Modules/ei_sample/build directory

--- Legacy_Modules/ei_sample/build/image_preload.cmake
+++
@@ -1,4 +0,0 @@
-# Generated file that can be used to preload variant images
-set(CACHED_CONF_FILE "/home/moho/Documents/testbase/ei_sample/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "/home/moho/Documents/testbase/ei_sample/boards/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "/usr/bin/python3" CACHE INTERNAL "NCS child image controlled")
